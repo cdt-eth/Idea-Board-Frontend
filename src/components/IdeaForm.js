a63eeae04826084e85f9969d0d0046cf3e97bcd4
christian <christiandavidphoto@gmail.com>
2018-05-31 16:05:36 -0400

    IdeaForm text inputting

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -4,15 +4,37 @@ class IdeaForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      title: this.props.idea.title,
+      body: this.props.idea.body
+    };
   }
+
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
   render() {
     return (
       <div className="title">
         <form>
-          <input className="input" type="text" name="title" placeholder="Enter a title" />
-          <textarea className="input" name="body" placeholder="Describe your idea" />
+          <input
+            className="input"
+            type="text"
+            name="title"
+            placeholder="Enter a title"
+            value={this.state.title}
+            onChange={this.handleInput}
+          />
+          <textarea
+            className="input"
+            name="body"
+            placeholder="Describe your idea"
+            value={this.state.body}
+            onChange={this.handleInput}
+          />
         </form>
       </div>
     );
